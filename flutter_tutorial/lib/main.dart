570e2810f14f83bc8873df1fdd703bcfbec01c10
Austin Keiber <adkeiber@gmail.com>
2021-03-02 19:26:17 -0500

    End of day 2
    Added stateful widget that allows for changing of the quiz's question

--- flutter_tutorial/lib/main.dart
+++ flutter_tutorial/lib/main.dart
@@ -4,15 +4,52 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+
+  void _answerQuestion() {
+    setState(() {
+      questionIndex++;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      "What's your favorite color?",
+      "What's your favorite animal?"
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text("My First Applicatoin"),
         ),
-        body: Text("This is the body"),
+        body: Column(
+          children: [
+            Text(
+              questions[questionIndex],
+            ),
+            RaisedButton(
+              child: Text("Answer one"),
+              onPressed: _answerQuestion,
+            ),
+            RaisedButton(
+              child: Text("Answer two"),
+              onPressed: _answerQuestion,
+            ),
+            RaisedButton(
+              child: Text("Answer three"),
+              onPressed: _answerQuestion,
+            ),
+          ],
+        ),
       ),
     );
   }
