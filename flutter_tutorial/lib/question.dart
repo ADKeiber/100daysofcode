d3c80348a5bd57e563b84569cdaf15b0fef5955a
Austin Keiber <adkeiber@gmail.com>
2021-03-03 22:48:30 -0500

    Question refactoring
    refactor question widget and add some styling to it

---
+++ flutter_tutorial/lib/question.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Question extends StatelessWidget {
+  //
+  final String questionTxt;
+  Question(this.questionTxt);
+  //
+  //
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text(
+        questionTxt,
+        style: TextStyle(
+          fontSize: 28,
+        ),
+        textAlign: TextAlign.center,
+      ),
+      width: double.infinity,
+      margin: EdgeInsets.all(10),
+    );
+  }
+}
