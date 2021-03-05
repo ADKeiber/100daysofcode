4cf39e7380a8a0414266b7883df5cbfc176fe98a
Austin Keiber <adkeiber@gmail.com>
2021-03-04 22:27:52 -0500

    Reset Button
    Final commit for day 4. Adds a reset button on the end screen to start the quiz over again.

--- flutter_tutorial/lib/result.dart
+++ flutter_tutorial/lib/result.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
-  int myScore;
-
-  Result(this.myScore);
+  final int myScore;
+  final Function restart;
+  Result(this.myScore, this.restart);
   String get resultPhrase {
     var resultText = "You did it!";
 
@@ -20,13 +20,23 @@ class Result extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text(
-        resultPhrase,
-        style: TextStyle(
-          fontSize: 36,
-          fontWeight: FontWeight.bold,
-        ),
-        textAlign: TextAlign.center,
+      child: Column(
+        children: [
+          Text(
+            resultPhrase,
+            style: TextStyle(
+              fontSize: 36,
+              fontWeight: FontWeight.bold,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          FlatButton(
+            child: Text("Restart Quiz!!"),
+            onPressed: restart,
+            textColor: Colors.white,
+            color: Colors.blue,
+          ),
+        ],
       ),
     );
   }
