a375a53e5888f88bfa44ce3822c8c512c779d002
Austin Keiber <adkeiber@gmail.com>
2021-03-08 21:15:53 -0500

    Replaced the listview contents
    changed the list view contents to be cards with list tiles in them instead of rows with a container and a column in it

--- finance_tracker/lib/widgets/transaction_list.dart
+++ finance_tracker/lib/widgets/transaction_list.dart
@@ -31,48 +31,76 @@ class TransactionList extends StatelessWidget {
           : ListView.builder(
               itemBuilder: (ctx, index) {
                 return Card(
-                  child: Row(
-                    children: [
-                      Container(
-                        child: Text(
-                          " \$${_userTransactions[index].cost.toStringAsFixed(2)}",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                            color: Theme.of(context).primaryColor,
+                  elevation: 5,
+                  margin: EdgeInsets.symmetric(
+                    vertical: 8,
+                    horizontal: 5,
+                  ),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 30,
+                      child: Padding(
+                        padding: EdgeInsets.all(5),
+                        child: FittedBox(
+                          child: Text(
+                            "\$${_userTransactions[index].cost}",
                           ),
                         ),
-                        margin: EdgeInsets.symmetric(
-                          vertical: 10,
-                          horizontal: 15,
-                        ),
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Theme.of(context).primaryColor,
-                            width: 2,
-                          ),
-                        ),
-                        padding: EdgeInsets.all(10),
                       ),
-                      Column(
-                        children: [
-                          Text(
-                            _userTransactions[index].title,
-                            style: Theme.of(context).textTheme.headline6,
-                          ),
-                          Text(
-                            DateFormat('MM/dd/yyyy')
-                                .format(_userTransactions[index].date),
-                            style: TextStyle(
-                              color: Colors.grey,
-                            ),
-                          ),
-                        ],
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                      ),
-                    ],
+                    ),
+                    title: Text(
+                      _userTransactions[index].title,
+                      style: Theme.of(context).textTheme.headline6,
+                    ),
+                    subtitle: Text(
+                      DateFormat.yMMMd().format(_userTransactions[index].date),
+                    ),
                   ),
                 );
+
+                //   Card(
+                //     child: Row(
+                //       children: [
+                //         Container(
+                //           child: Text(
+                //             " \$${_userTransactions[index].cost.toStringAsFixed(2)}",
+                //             style: TextStyle(
+                //               fontWeight: FontWeight.bold,
+                //               fontSize: 20,
+                //               color: Theme.of(context).primaryColor,
+                //             ),
+                //           ),
+                //           margin: EdgeInsets.symmetric(
+                //             vertical: 10,
+                //             horizontal: 15,
+                //           ),
+                //           decoration: BoxDecoration(
+                //             border: Border.all(
+                //               color: Theme.of(context).primaryColor,
+                //               width: 2,
+                //             ),
+                //           ),
+                //           padding: EdgeInsets.all(10),
+                //         ),
+                //         Column(
+                //           children: [
+                //             Text(
+                //               _userTransactions[index].title,
+                //               style: Theme.of(context).textTheme.headline6,
+                //             ),
+                //             Text(
+                //               DateFormat('MM/dd/yyyy')
+                //                   .format(_userTransactions[index].date),
+                //               style: TextStyle(
+                //                 color: Colors.grey,
+                //               ),
+                //             ),
+                //           ],
+                //           crossAxisAlignment: CrossAxisAlignment.start,
+                //         ),
+                //       ],
+                //     ),
+                //   );
               },
               itemCount: _userTransactions.length,
             ),
