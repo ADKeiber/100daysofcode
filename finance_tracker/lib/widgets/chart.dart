6d5940629d4f7c2dddda385b04cf847c54128375
Austin Keiber <adkeiber@gmail.com>
2021-03-08 21:20:07 -0500

    Bar Order and day representation
    Switch order of the bars so most recent day (today) is on the right. Added 3 letters for representing a day instead of a letter

--- finance_tracker/lib/widgets/chart.dart
+++ finance_tracker/lib/widgets/chart.dart
@@ -25,10 +25,10 @@ class Chart extends StatelessWidget {
       }
 
       return {
-        "day": DateFormat.E().format(weekDay).substring(0, 1),
+        "day": DateFormat.E().format(weekDay).substring(0, 3),
         "cost": totalSum,
       };
-    });
+    }).reversed.toList();
   }
 
   double get totalSpending {
