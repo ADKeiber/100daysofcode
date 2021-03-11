572a3825f96e94c6e4901cd1880e487c5ad1d91b
Austin Keiber <adkeiber@gmail.com>
2021-03-10 23:12:44 -0500

    ADDING COMMENTS
    Finally.. I know its a long time coming but I'm going through and adding comments

--- finance_tracker/lib/main.dart
+++ finance_tracker/lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:finance_tracker/widgets/adaptive_appbar.dart';
 import 'package:finance_tracker/widgets/adaptive_scaffold.dart';
 import 'package:flutter/material.dart';
@@ -11,25 +9,27 @@ import './widgets/new_transaction.dart';
 import './models/transaction.dart';
 import './widgets/chart.dart';
 
-void main() {
-  // WidgetsFlutterBinding.ensureInitialized();
-  // SystemChrome.setPreferredOrientations(
-  //   [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown],
-  // );
-  runApp(MyApp());
-}
+//Call to run the application
+void main() => runApp(MyApp());
 
+//Main application widget
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    //creates the main MaterialApp for the entire program
     return MaterialApp(
+      //sets title
       title: 'Personal Expenses',
+      //sets theme data where main is purple accent is blue and the error is dark
+      //red
       theme: ThemeData(
         primarySwatch: Colors.purple,
         accentColor: Colors.blue[200],
         errorColor: Colors.red[700],
+        //sets main font to be used as imported font "quicksand"
         fontFamily: "Quicksand",
         appBarTheme: AppBarTheme(
+          //adds opensands font to the theme of the app bar
           textTheme: ThemeData.light().textTheme.copyWith(
                 headline6: TextStyle(
                   fontFamily: 'OpenSans',
@@ -41,25 +41,29 @@ class MyApp extends StatelessWidget {
                 ),
               ),
         ),
+        //adds opensans to be the main font of the entire app
         textTheme: ThemeData.light().textTheme.copyWith(
               headline6: TextStyle(
-                fontFamily: "Open Sans",
+                fontFamily: "OpenSans",
                 fontWeight: FontWeight.bold,
                 fontSize: 18,
               ),
             ),
       ),
+      //sets the home widget to MyHomePage stateful widget
       home: MyHomePage(),
     );
   }
 }
 
+//Main home widget
 class MyHomePage extends StatefulWidget {
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  //method that pops up modal where you can add a new transaction
   void _startAddNewTransaction(BuildContext ctx) {
     showModalBottomSheet(
       context: ctx,
@@ -69,13 +73,18 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
+  //method where transaction is deleted based on its ID
   void _deleteTransaction(String id) {
     setState(() {
       _userTransactions.removeWhere((tx) => tx.id == id);
     });
   }
 
+  //list filled with transactions that will populate a listview widget
   final List<Transaction> _userTransactions = [];
+
+  //method that takes in a title, cost, and date to create a new transation that
+  //will be added to the the _userTransactions list
   void _addNewTransaction(String txTitle, double txCost, DateTime chosenDate) {
     final newTx = Transaction(
       id: DateTime.now().toString(),
@@ -88,6 +97,8 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  //bool used with a switch to determine if the chart or list is shown while
+  // in landscape mode
   bool _showChart = false;
   List<Transaction> get _recentTransactions {
     return _userTransactions.where((tx) {
@@ -101,12 +112,16 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    //gets the media query of the application
     final mediaQuery = MediaQuery.of(context);
+    //stores boolean True if orientation is landscape and false otherwise
     final isLandscape = mediaQuery.orientation == Orientation.landscape;
 
+    //adaptive appbar that takes in add new transaction and context of app
     final PreferredSizeWidget appBar =
         AdaptiveAppBar(_startAddNewTransaction, context);
 
+    //creates a widget that is a sized Transaction list view
     final txListWidget = Container(
       height: (mediaQuery.size.height -
               appBar.preferredSize.height -
@@ -114,10 +129,15 @@ class _MyHomePageState extends State<MyHomePage> {
           0.7,
       child: TransactionList(_userTransactions, _deleteTransaction),
     );
+
+    //creates a pagebody that contains a scrollview with a chart, list of transactions
+    //and a floating button
     final pageBody = SafeArea(
       child: SingleChildScrollView(
         child: Column(
           children: [
+            //when in landscape mode generate a switch that either shows
+            // the chart or the list of transactions
             if (isLandscape)
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
@@ -129,6 +149,8 @@ class _MyHomePageState extends State<MyHomePage> {
                   Switch.adaptive(
                     activeColor: Theme.of(context).accentColor,
                     value: _showChart,
+
+                    //toggles between chart and list of transactions
                     onChanged: (val) {
                       setState(() {
                         _showChart = val;
@@ -137,6 +159,8 @@ class _MyHomePageState extends State<MyHomePage> {
                   ),
                 ],
               ),
+
+            //if in portrait mode adds a chart to the view
             if (!isLandscape)
               Container(
                 height: (mediaQuery.size.height -
@@ -145,7 +169,11 @@ class _MyHomePageState extends State<MyHomePage> {
                     0.3,
                 child: Chart(_recentTransactions),
               ),
+            //if in portrait mode adds the list of transactoins to the view
             if (!isLandscape) txListWidget,
+
+            //if in landscape mode determine if chart or list of transactions
+            //should be shown depending on the value of _showChart
             if (isLandscape)
               _showChart
                   ? Container(
@@ -162,6 +190,8 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
     );
+    //adds an adaptive scaffold that uses the appbar, pagebody, and function
+    //that adds a new transaction to list view
     return AdaptiveScaffold(appBar, pageBody, _startAddNewTransaction);
   }
 }
