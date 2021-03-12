b430e27fdbb64072852469c6dd14f9e4de609962
Austin Keiber <adkeiber@gmail.com>
2021-03-11 19:40:22 -0500

    more comments
     added comments to new transaction modal

--- finance_tracker/lib/widgets/new_transaction.dart
+++ finance_tracker/lib/widgets/new_transaction.dart
@@ -5,7 +5,9 @@ import 'package:flutter/cupertino.dart';
 
 import './adaptive_button.dart';
 
+// widget used as a modal that allows for the user to create a new transaction
 class NewTransaction extends StatefulWidget {
+  //function used to submit a new transaction
   final Function submitTrans;
 
   NewTransaction(this.submitTrans);
@@ -15,12 +17,15 @@ class NewTransaction extends StatefulWidget {
 }
 
 class _NewTransactionState extends State<NewTransaction> {
+  //text controller that takes a user inputed title and amount for a transaction
   final titleController = TextEditingController();
-
   final amountController = TextEditingController();
 
+  //Variables used to store a date and is originally set to the current day
   DateTime _selectedDate = DateTime.now();
 
+  //function that takes the title controller and amountcontroller and
+  //adds them to transactions and closes the modal
   void _submitData() {
     final enteredTitle = titleController.text;
     final enteredAmount = double.parse(amountController.text);
@@ -32,9 +37,11 @@ class _NewTransactionState extends State<NewTransaction> {
       enteredAmount,
       _selectedDate,
     );
+    //closes modal
     Navigator.of(context).pop();
   }
 
+  //function that allows for user to pick a date the transaction occured
   void _presentDatePicker() {
     showDatePicker(
       context: context,
@@ -53,16 +60,17 @@ class _NewTransactionState extends State<NewTransaction> {
 
   @override
   Widget build(BuildContext context) {
+    //notification listener turns off web glow
     return NotificationListener<OverscrollIndicatorNotification>(
       onNotification: (overScroll) {
         overScroll.disallowGlow();
         return;
       },
+      //scroll view that allows for modal to not be covered up by keyboard
       child: SingleChildScrollView(
         child: Card(
           elevation: 10,
           child: Container(
-            // height: MediaQuery.of(context).size.height * .5,
             padding: EdgeInsets.only(
               top: 10,
               left: 10,
@@ -71,17 +79,21 @@ class _NewTransactionState extends State<NewTransaction> {
             ),
             child: Column(
               children: [
+                //contains Title information for new transaction
                 TextField(
                   decoration: InputDecoration(labelText: "Title"),
                   controller: titleController,
                   onSubmitted: (_) => _submitData(),
                 ),
+                //contains amount/cost information for new transaction
                 TextField(
                   decoration: InputDecoration(labelText: "Amount"),
                   controller: amountController,
                   keyboardType: TextInputType.number,
                   onSubmitted: (_) => _submitData(),
                 ),
+                //contains date picker that is fisrt set to todays date but
+                //can be updated by the user using a date picker
                 Container(
                   height: 70,
                   child: Row(
@@ -95,6 +107,8 @@ class _NewTransactionState extends State<NewTransaction> {
                     ],
                   ),
                 ),
+                //button used to add transaction
+                //shadowing for text button
                 ClipRRect(
                   borderRadius: BorderRadius.circular(5.0),
                   child: Container(
@@ -113,6 +127,7 @@ class _NewTransactionState extends State<NewTransaction> {
                         ),
                       ],
                     ),
+                    //actual text button construction
                     child: TextButton(
                       child: Text(
                         "Add transaction",
