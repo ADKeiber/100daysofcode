cda20e8d7777f8a5cd1324f02be5287473366749
Austin Keiber <adkeiber@gmail.com>
2021-03-11 20:14:09 -0500

    more comments
    added comments to the chart bar widget

--- finance_tracker/lib/widgets/chart_bar.dart
+++ finance_tracker/lib/widgets/chart_bar.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class ChartBar extends StatelessWidget {
+  //contains all the information for each bar in the chart
   final String label;
   final double spendingAmount;
   final double spendingPercentOfTotal;
@@ -9,10 +10,12 @@ class ChartBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    //layoutbuilder has the ability to detect heights
     return LayoutBuilder(
       builder: (ctx, contraints) {
         return Column(
           children: [
+            //contains the spending amount for that day
             Container(
               height: contraints.maxHeight * 0.15,
               child: FittedBox(
@@ -21,9 +24,12 @@ class ChartBar extends StatelessWidget {
                 ),
               ),
             ),
+            //spacing box
             SizedBox(
               height: contraints.maxHeight * 0.05,
             ),
+            //contains the actual bar itself with percentage spent that day
+            //compared to the entire week
             Container(
               height: contraints.maxHeight * 0.6,
               width: 10,
@@ -36,6 +42,7 @@ class ChartBar extends StatelessWidget {
                       borderRadius: BorderRadius.circular(20),
                     ),
                   ),
+                  //contains the "fill" of the bar representing the percentage
                   FractionallySizedBox(
                     heightFactor: spendingPercentOfTotal,
                     child: Container(
@@ -48,9 +55,11 @@ class ChartBar extends StatelessWidget {
                 ],
               ),
             ),
+            //spacer
             SizedBox(
               height: contraints.maxHeight * 0.05,
             ),
+            //contains the first 3 letters for that day
             Container(
               height: contraints.maxHeight * 0.15,
               child: FittedBox(
