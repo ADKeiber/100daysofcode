24c1b93db550984645a0adb7736b6d4bd595f38a
Austin Keiber <adkeiber@gmail.com>
2021-03-13 23:01:05 -0500

    add new assets
    add new fonts and images

--- basics/lib/main.dart
+++ basics/lib/main.dart
@@ -14,6 +14,9 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        textTheme: Theme.of(context).textTheme.apply(
+              fontFamily: 'Open Sans',
+            ),
       ),
       home: HomeView(),
     );
